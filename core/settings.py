79fcea0019646b5d139838663857e4737ab7df20
Mr3404 <aliyazkhanov@gmail.com>
2026-02-25 16:09:07 +0400

    app dockerized

--- core/settings.py
+++ core/settings.py
@@ -54,15 +54,15 @@ REST_FRAMEWORK = {
     "DATETIME_FORMAT": "%d-%m-%Y %H:%M",
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 
 
 SIMPLE_JWT = {
